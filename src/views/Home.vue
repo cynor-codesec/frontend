92f3ec436a2b598b72e9bd729bc65ca02e75b875
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2021-10-07 19:28:09 +1030

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-button variant="success">Bootstrap Button</b-button>
   </div>
 </template>
 
